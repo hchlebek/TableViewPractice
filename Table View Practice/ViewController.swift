0e037d396da3464d41112ab28ebaa27595ddfbde
HChlebek <student@hchlebek.local>
2016-01-13 10:55:20 -0600

    Initial Commit

---
+++ Table View Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Table View Practice
+//
+//  Created by HChlebek on 1/13/16.
+//  Copyright © 2016 HChlebek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
